21ef0ecab0cdbba06e5b1f9c2dab9307264aa9be
NattapongSuetrong <153727825+NattapongSuetrong@users.noreply.github.com>
2024-07-17 16:17:52 +0700

    Update main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI, HTTPException # type: ignore
 from sqlalchemy.orm import Session # type: ignore
 from typing import List
 import models
-from schemas import UserCreate, UserRead, ShrimpQuantityCreate, ShrimpQuantityRead # type: ignore
+from schemas import UserCreate, UserRead, ShrimpQuantityCreate, ShrimpQuantityRead, ShrimpPondCreate, ShrimpPondRead, ShrimpPondUpdate # type: ignore
 from database import SessionLocal, engine # type: ignore
 
 # Create all tables defined in models
@@ -28,10 +28,10 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 # Read all users
-# @app.get("/users/", response_model=List[UserRead])
-# def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-#     users = db.query(models.User).offset(skip).limit(limit).all()
-#     return users
+@app.get("/users/", response_model=List[UserRead])
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    users = db.query(models.User).offset(skip).limit(limit).all()
+    return users
 
 # Read a user by ID
 @app.get("/users/{user_id}", response_model=UserRead)
@@ -112,3 +112,48 @@ def delete_shrimp_quantity(shrimp_quantity_id: int, db: Session = Depends(get_db
     db.delete(db_shrimp_quantity)
     db.commit()
     return {"detail": "Shrimp quantity deleted"}
+
+#Create shrimp pond
+@app.post("/shrimp_ponds/", response_model=ShrimpPondRead)
+def create_shrimp_pond(shrimp_pond: ShrimpPondCreate, db: Session = Depends(get_db)):
+    db_shrimp_pond = models.ShrimpPond(pond_name=shrimp_pond.pond_name, user_id=shrimp_pond.user_id,pond_quantity=shrimp_pond.pond_quantity, shrimp_sold=shrimp_pond.shrimp_sold)
+    db.add(db_shrimp_pond)
+    db.commit()
+    db.refresh(db_shrimp_pond)
+    return db_shrimp_pond
+
+#Read shrimp pond
+@app.get("/shrimp_ponds/{user_id}", response_model=List[ShrimpPondRead])
+def read_shrimp_ponds(user_id: int, db: Session = Depends(get_db)):
+    db_shrimp_ponds = db.query(models.ShrimpPond).filter(models.ShrimpPond.user_id == user_id).all()
+    if not db_shrimp_ponds:
+        raise HTTPException(status_code=404, detail="Shrimp ponds not found")
+    return db_shrimp_ponds
+
+# Update shrimp pond
+@app.put("/shrimp_ponds/{pond_id}", response_model=ShrimpPondRead)
+def update_shrimp_pond(pond_id: int, shrimp_pond: ShrimpPondUpdate, db: Session = Depends(get_db)):
+    db_shrimp_pond = db.query(models.ShrimpPond).filter(models.ShrimpPond.id == pond_id).first()
+    if db_shrimp_pond is None:
+        raise HTTPException(status_code=404, detail="Shrimp quantity not found")
+    db_shrimp_pond.pond_name = shrimp_pond.pond_name
+    db_shrimp_pond.pond_quantity = shrimp_pond.pond_quantity
+    db_shrimp_pond.shrimp_sold = shrimp_pond.shrimp_sold
+    db_shrimp_pond.date = shrimp_pond.date
+    db.commit()
+    db.refresh(db_shrimp_pond)
+    return db_shrimp_pond
+
+# Delete shrimp pond
+@app.delete("/shrimp_ponds/{pond_id}")
+def delete_shrimp_pond(pond_id: int, db: Session = Depends(get_db)):
+    db_shrimp_pond = db.query(models.ShrimpPond).filter(models.ShrimpPond.id == pond_id).first()
+    if db_shrimp_pond is None:
+        raise HTTPException(status_code=404, detail="Shrimp quantity not found")
+    db.delete(db_shrimp_pond)
+    db.commit()
+    return {"detail": "Shrimp quantity deleted"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8888)
