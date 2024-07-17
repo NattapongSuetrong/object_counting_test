af497eea7ef27d29532dcc48ddec824c8a5ce991
NattapongSuetrong <153727825+NattapongSuetrong@users.noreply.github.com>
2024-07-17 16:18:21 +0700

    Update models.py

--- sql_app/models.py
+++ sql_app/models.py
@@ -2,8 +2,13 @@ from datetime import datetime, timezone
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime # type: ignore
 from sqlalchemy.orm import relationship # type: ignore
 from sqlalchemy.ext.declarative import declarative_base # type: ignore
+import pytz
 
 Base = declarative_base()
+timezone_utc_7 = pytz.timezone('Asia/Bangkok')
+
+def current_time_utc_7():
+    return datetime.now(timezone_utc_7)
 
 class User(Base):
     __tablename__ = "users"
@@ -14,6 +19,7 @@ class User(Base):
     hashed_password = Column(String)
 
     shrimp_quantities = relationship("ShrimpQuantity", back_populates="owner")
+    shrimp_pond = relationship("ShrimpPond", back_populates="owner_pond")
 
 class ShrimpQuantity(Base):
     __tablename__ = "shrimp_quantities"
@@ -21,6 +27,19 @@ class ShrimpQuantity(Base):
     id = Column(Integer, primary_key=True, index=True)
     quantity = Column(Integer)
     user_id = Column(Integer, ForeignKey("users.id"))
-    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))
-
+    # timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    timestamp = Column(DateTime, default=current_time_utc_7)
+    
     owner = relationship("User", back_populates="shrimp_quantities")
+    
+class ShrimpPond(Base):
+    __tablename__ = "shrimp_pond"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    pond_name = Column(String, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    pond_quantity = Column(Integer, index=True)
+    shrimp_sold = Column(Integer, default=None)
+    date = Column(DateTime, default=current_time_utc_7)
+
+    owner_pond = relationship("User", back_populates="shrimp_pond")
